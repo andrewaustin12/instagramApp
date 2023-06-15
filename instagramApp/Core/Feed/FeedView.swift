8d4087b451ae1152c17a5b30465e8dcdf426fb83
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-06-15 11:04:30 +0200

    added feed view

---
+++ instagramApp/Core/Feed/FeedView.swift
@@ -0,0 +1,42 @@
+//
+//  FeedView.swift
+//  instagramApp
+//
+//  Created by andrew austin on 6/15/23.
+//
+
+import SwiftUI
+
+struct FeedView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                LazyVStack(spacing: 32) {
+                    ForEach(0 ... 10, id: \.self) { post in
+                        FeedCell()
+                    }
+                }
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("instagram-logo-black")
+                        .resizable()
+                        .frame(width: 100, height: 35)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "paperplane")
+                        .imageScale(.large)
+                }
+            }
+        }
+    }
+}
+
+struct FeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedView()
+    }
+}
