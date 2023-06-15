b5141aaf2db6da79eb793bbf488f5032420d2283
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-06-15 16:41:08 +0200

    added complete sign flows

---
+++ instagramApp/Core/Authentication/CompleteSignUpView.swift
@@ -0,0 +1,61 @@
+//
+//  CompleteSignUpView.swift
+//  instagramApp
+//
+//  Created by andrew austin on 6/15/23.
+//
+
+import SwiftUI
+
+struct CompleteSignUpView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack(spacing: 12) {
+            Spacer()
+            
+            Text("Welcome to Instagram, Andy")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding(.top)
+                .multilineTextAlignment(.center)
+            
+            Text("Click below to complete registration")
+                .font(.footnote)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 24)
+
+            
+            Button {
+                print("Complete Sign up")
+            } label: {
+                Text("Complete Sign Up")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                    .frame(width: 360, height: 44)
+                    .background(Color(.systemBlue))
+                    .cornerRadius(8)
+            }
+            .padding(.vertical)
+            
+            
+            Spacer()
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Image(systemName: "chevron.left")
+                    .imageScale(.large)
+                    .onTapGesture {
+                        dismiss()
+                    }
+            }
+        }
+    }
+}
+
+struct CompleteSignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        CompleteSignUpView()
+    }
+}
