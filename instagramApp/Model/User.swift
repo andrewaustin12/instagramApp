af1df0f896ad6275b6292a3991642a4efe0896aa
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-06-16 13:36:29 +0200

    Created user model with mock user data

---
+++ instagramApp/Model/User.swift
@@ -0,0 +1,27 @@
+//
+//  User.swift
+//  instagramApp
+//
+//  Created by andrew austin on 6/16/23.
+//
+
+import Foundation
+
+struct User: Identifiable, Codable {
+    let id: String
+    var username: String
+    var profileImageUrl: String?
+    var fullname: String?
+    var bio: String?
+    let email: String
+}
+
+extension User {
+    static var MOCK_USERS: [User] = [
+        .init(id: NSUUID().uuidString, username: "batman", profileImageUrl: "batman-1", fullname: "Bruce Wayne", bio: "aka the bat", email: "batman@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "venom", profileImageUrl: "venom-1", fullname: "Eddie Brock", bio: "Venom", email: "venom@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "ironman", profileImageUrl: "ironman-1", fullname: "Tony Stark", bio: "aka the ironman", email: "ironman@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "blackpanther", profileImageUrl: "black-panther-1", fullname: "Black Panther", bio: "wakanda forever", email: "blackpanther@gmail.com"),
+        .init(id: NSUUID().uuidString, username: "spiderman", profileImageUrl: "spiderman-1", fullname: "Peter Parker", bio: "Your firendly neighborhood hero", email: "spiderman@gmail.com"),
+    ]
+}
