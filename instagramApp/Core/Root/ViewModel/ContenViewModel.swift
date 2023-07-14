8f4dace5de9d559df4e9ac8e6828716927e79e6d
andrewaustin12 <andrew.d.austin2@gmail.com>
2023-07-14 17:42:16 +0700

    Setup Firebase Authentication Service

---
+++ instagramApp/Core/Root/ViewModel/ContenViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ContenViewModel.swift
+//  instagramApp
+//
+//  Created by andrew austin on 7/14/23.
+//
+
+import Foundation
+import Firebase
+import Combine
+
+class ContentViewModel: ObservableObject {
+    
+    private let service = AuthService.shared
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var userSession: FirebaseAuth.User?
+    
+    init() {
+        setupSubscribers()
+    }
+    
+    func setupSubscribers() {
+        service.$userSession.sink { [weak self] userSession in
+            self?.userSession = userSession
+        }
+        .store(in: &cancellables)
+    }
+}
